fd6dffe029da0914502ca125cd2c2f778feaf8e6
Milen Georgiev <milen.georgiev@payarc.com>
2024-10-25 00:25:50 +0300

    init

---
+++ src/app/page.js
@@ -0,0 +1,52 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.page}>
+      <main className={styles.main}>
+        <h1 data-sel="title">Welcome</h1>
+        <ol data-sel="list">
+          <li>Have a cup of coffee.</li>
+          <li>Get some dark chocolate.</li>
+          <li>Don't over-work yourself.</li>
+        </ol>
+
+        <div className={styles.ctas}>
+          <a
+            className={styles.primary}
+            href=""
+            target="_blank"
+            rel="noopener noreferrer"
+            data-sel="go"
+          >
+            Go
+          </a>
+        </div>
+      </main>
+      <footer className={styles.footer}>
+        <a
+          href="#"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Eat
+        </a>
+        <a
+          href="#"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Sleep
+        </a>
+        <a
+          href="#"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Play
+        </a>
+      </footer>
+    </div>
+  );
+}
