e336be6c06e3fd84ec48a0665bf630c417984689
Milen Georgiev <milen.georgiev@payarc.com>
2024-11-01 11:40:43 +0200

    fix

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,8 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <h1 data-sel="title">Welcome to the jungle!</h1>
+        <h1 data-sel="title">Welcome</h1>
+        <p>to the jungle</p>
         <ol data-sel="list">
           <li>Have a cup of coffee.</li>
           <li>Get some dark chocolate.</li>
